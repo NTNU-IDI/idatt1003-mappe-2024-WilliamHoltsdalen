a50960a65d2dfa07201b76bbc2d595071ffdcde5
WilliamHoltsdalen <williamlh@hotmail.no>
2024-11-03 15:47:33 +0100

    Change Grocery class to have list of batches

--- src/main/java/edu/ntnu/idi/idatt/Grocery.java
+++ src/main/java/edu/ntnu/idi/idatt/Grocery.java
@@ -1,6 +1,8 @@
 package edu.ntnu.idi.idatt;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * A {@code Grocery} object represents a grocery item.
@@ -11,20 +13,18 @@ import java.time.LocalDate;
  *      set at object creation and cannot be changed afterward.
  * <li>{@code category} A String representing the category of the grocery item. This field is
  *      immutable and assigned at object creation.
- * <li>{@code amount} A double representing the quantity of the grocery item. This field is mutable
+ * <li>{@code totalAmount} A double representing the total quantity of the grocery item. This field is mutable
  *      and can be updated throughout the grocery item's lifecycle.
  * <li>{@code unit} A String representing the unit of measurement for the grocery item's amount.
  *      This field is immutable, set at object creation, and unmodifiable thereafter.
- * <li>{@code expirationDate} A LocalDate representing the expiration date of the grocery item.
- *      This field is immutable and does not change after object creation.
- * <li>{@code price} A double representing the price of the grocery item. This field is immutable
- *      and remains constant after creation.
- * <li>{@code currency} A String representing the currency associated with the price. This field
- *      is immutable, linked to the fixed price of the item.
+ * <li> {@code batches} A list of {@code GroceryBatch} objects representing the batches of the
+ *      grocery item. This field is mutable and can be updated throughout the grocery
+ *     item's lifecycle.
  * </ul>
  * <p>
  * Each field has an accessor method to get the value of the field.
- * The field {@code amount} has a mutator method to set the value of the field.
+ * The fields {@code totalAmount} and {@code batches} have mutator methods to change the values of
+ * the fields.
  * <p>
  * The class constructor validates and initializes all fields. When given an invalid value
  * (such as a negative amount or price, a null or empty string, or a null expiration date), the
@@ -33,11 +33,9 @@ import java.time.LocalDate;
 public class Grocery {
   private final String name;
   private final String category;
-  private double amount;
+  private double totalAmount;
   private final String unit;
-  private final LocalDate expirationDate;
-  private final double price;
-  private final String currency;
+  private final List<GroceryBatch> batches;
 
   /**
    * Creates a new grocery item.
@@ -47,19 +45,11 @@ public class Grocery {
    *
    * @param category specifies the category of the grocery item. Category must not equal
    *                 {@code null} or an empty string.
-   * @param amount specifies the amount of the grocery item. Amount must equal a positive number.
    * @param unit specifies the unit of the grocery item. Unit must not equal {@code null} or an
    *             empty string.
-   * @param expirationDate specifies the expiration date of the grocery item. Expiration date must
-   *                       not equal {@code null}.
-   * @param price specifies the price of the grocery item. Price must equal a positive number.
-   * @param currency specifies the currency of the grocery item. Currency must not equal
-   *                 {@code null} or an empty string.
-   *
    * @throws IllegalArgumentException if any parameters violate the constraints specified.
    */
-  public Grocery(String name, String category, double amount, String unit, LocalDate expirationDate,
-                 double price, String currency) throws IllegalArgumentException {
+  public Grocery(String name, String category, String unit) throws IllegalArgumentException {
     // Guard clauses
     if (name == null || name.isBlank()) {
       throw new IllegalArgumentException("Name cannot be null or blank");
@@ -67,30 +57,16 @@ public class Grocery {
     if (category == null || category.isBlank()) {
       throw new IllegalArgumentException("Category cannot be null or blank");
     }
-    if (amount <= 0) {
-      throw new IllegalArgumentException("Amount must be a positive number");
-    }
     if (unit == null || unit.isBlank()) {
       throw new IllegalArgumentException("Unit cannot be null or blank");
-    }
-    if (expirationDate == null) {
-      throw new IllegalArgumentException("Expiration date cannot be null");
-    }
-    if (price <= 0) {
-      throw new IllegalArgumentException("Price must be a positive number");
-    }
-    if (currency == null || currency.isBlank()) {
-      throw new IllegalArgumentException("Currency cannot be null or blank");
     }
 
     // Assigning valid values
     this.name = name;
     this.category = category;
-    this.amount = amount;
     this.unit = unit;
-    this.expirationDate = expirationDate;
-    this.price = price;
-    this.currency = currency;
+
+    this.batches = new ArrayList<>();
   }
 
   /**
@@ -110,8 +86,8 @@ public class Grocery {
   /**
    * @return The amount of the grocery item.
    */
-  public double getAmount() {
-    return amount;
+  public double getTotalAmount() {
+    return totalAmount;
   }
 
   /**
@@ -122,49 +98,91 @@ public class Grocery {
   }
 
   /**
-   * @return The expiration date of the grocery item.
+   * @return The purchase history of the grocery item.
    */
-  public LocalDate getExpDate() {
-    return expirationDate;
-  }
-
-  /**
-   * @return The price of the grocery item.
-   */
-  public double getPrice() {
-    return price;
-  }
-
-  /**
-   * @return The currency of the grocery item.
-   */
-  public String getCurrency() {
-    return currency;
+  public List<GroceryBatch> getBatches() {
+    return batches;
   }
 
   /**
    * Sets the amount of the grocery item.
    *
-   * @param amount specifies the new amount of the grocery item. Amount must equal a positive
+   * @param newAmount specifies the new amount of the grocery item. Amount must equal a positive
    *               number.
    *
-   * @throws IllegalArgumentException if the amount violates the constraints specified.
+   * @throws IllegalArgumentException if the amount equals zero or a negative number.
    */
-  public void setAmount(double amount) {
-    if (amount <= 0) {
+  private void setTotalAmount(double newAmount) {
+    if (newAmount <= 0) {
       throw new IllegalArgumentException("Amount must be a positive number");
     }
-    this.amount = amount;
+    this.totalAmount = newAmount;
   }
 
   /**
-   * @return A string representation of the grocery item. Including {@code name}, {@code category},
-   *         {@code amount}, {@code unit}, {@code expirationDate}, {@code price}, and {@code currency}.
+   * Adds a new batch to the grocery item.
+   * <p>
+   * The batch is added to the batches list and the total amount of the grocery item is updated.
+   * The batches list is sorted by expiration when updated.
+   *
+   * @param batch specifies the batch to add. Batch must not equal {@code null}.
+   * @throws IllegalArgumentException if the batch is {@code null}.
+   */
+  public void addBatch(GroceryBatch batch) throws IllegalArgumentException {
+    if (batch == null) {
+      throw new IllegalArgumentException("Batch cannot be null");
+    }
+    batches.add(batch);
+    batches.sort(Comparator.comparing(GroceryBatch::getExpirationDate));
+    setTotalAmount(this.totalAmount + batch.getAmount());
+  }
+
+  /**
+   *
+   * @param amount specifies the amount of the grocery item to consume. Amount must equal a
+   *               positive number.
+   * @throws IllegalArgumentException if the amount to consume is greater than the total amount of the
+   *         grocery item, or if the amount to consume is zero or a negative number.
+   */
+  public void consume(double amount) {
+    if (amount <= 0) {
+      throw new IllegalArgumentException("Amount must be a positive number");
+    }
+    if (amount > this.totalAmount) {
+      throw new IllegalArgumentException("Amount to consume is greater than the total amount of the grocery item");
+    }
+
+    for (GroceryBatch batch : batches) {
+      if (batch.getAmount() == amount) {
+        batches.remove(batch);
+        setTotalAmount(this.totalAmount - amount);
+        return;
+      } else if (batch.getAmount() < amount) {
+        amount -= batch.getAmount();
+        batches.remove(batch);
+        setTotalAmount(this.totalAmount - batch.getAmount());
+      } else {
+        batch.setAmount(batch.getAmount() - amount);
+        setTotalAmount(this.totalAmount - amount);
+        return;
+      }
+    }
+  }
+
+  /**
+   * Overrides the default {@code toString} method, and returns a reader-friendly string
+   * representation of the grocery item.
+   *
+   * @return A string representation of the grocery item. Including name, category, amount, unit,
+   *        expiration date, and batch history.
    */
   @Override
   public String toString() {
-    return "Name: " + name + ", Category: " + category + ", Amount: "
-        + amount + " " + unit + ", Price: " + price + " " + currency + ", Expiration date: "
-        + expirationDate ;
+    StringBuilder str = new StringBuilder("Name: " + name + ", Category: " + category + ", Unit: "
+        + unit + ", totalAmount: " + totalAmount + " " + unit);
+    for (GroceryBatch batch : batches) {
+      str.append("\n").append(batch.toString());
+    }
+    return str.toString();
   }
 }
