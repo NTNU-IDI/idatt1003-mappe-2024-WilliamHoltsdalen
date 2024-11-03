26892ec8d8e0008e36025c05c5cf491955e9baa5
WilliamHoltsdalen <williamlh@hotmail.no>
2024-11-03 15:45:47 +0100

    Add GroceryBatch class

---
+++ src/main/java/edu/ntnu/idi/idatt/GroceryBatch.java
@@ -0,0 +1,54 @@
+package edu.ntnu.idi.idatt;
+
+import java.time.LocalDate;
+
+public class GroceryBatch {
+  private double amount;
+  private final double pricePerUnit;
+  private final LocalDate expirationDate;
+
+
+  public GroceryBatch(double amount, double pricePerUnit, LocalDate expirationDate) throws IllegalArgumentException {
+    if (amount <= 0) {
+      throw new IllegalArgumentException("Amount cannot be zero or a negative number.");
+    }
+    if (pricePerUnit < 0) {
+      throw new IllegalArgumentException("Price per unit cannot be a negative number.");
+    }
+    if (expirationDate == null) {
+      throw new IllegalArgumentException("Expiration date cannot be null.");
+    }
+
+    this.amount = amount;
+    this.pricePerUnit = pricePerUnit;
+    this.expirationDate = expirationDate;
+  }
+
+  public double getAmount() {
+    return amount;
+  }
+
+  public double getPricePerUnit() {
+    return pricePerUnit;
+  }
+
+  public LocalDate getExpirationDate() {
+    return expirationDate;
+  }
+
+
+  public void setAmount(double newAmount) {
+    if (newAmount <= 0) {
+      throw new IllegalArgumentException("Amount must be a positive number");
+    }
+    if (newAmount > this.amount) {
+      throw new IllegalArgumentException("Amount to consume is greater than the amount of the grocery batch");
+    }
+    this.amount = newAmount;
+  }
+
+  @Override
+  public String toString() {
+    return "Amount: " + amount +", Price per unit: " + pricePerUnit + ", Expiration date: " + expirationDate;
+  }
+}
