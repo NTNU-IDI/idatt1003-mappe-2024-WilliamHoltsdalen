904d6188e4f52b9a11ced745572f36eabc41197c
WilliamHoltsdalen <williamlh@hotmail.no>
2024-11-30 19:41:25 +0100

    Refactor FoodStorage class and add getter

--- src/main/java/edu/ntnu/idi/idatt/models/FoodStorage.java
+++ src/main/java/edu/ntnu/idi/idatt/models/FoodStorage.java
@@ -17,7 +17,13 @@ import java.util.List;
  */
 public class FoodStorage {
   /** The exception message for when a grocery object does not exist. */
-  private static final String NO_GROCERY_FOUND = "Grocery not found";
+  private static final String NO_GROCERY_FOUND_ERROR = "Grocery not found.";
+  private static final String NULL_GROCERY_ERROR = "Grocery cannot be null.";
+  private static final String GROCERY_EXISTS_ERROR = "Grocery already exists in storage.";
+  private static final String INVALID_NAME_ERROR = "Invalid name. Cannot be null or an empty string.";
+  private static final String INVALID_CATEGORY_ERROR = "Invalid category. Cannot be null or an empty string.";
+  private static final String INVALID_AMOUNT_ERROR = "Invalid amount.";
+  private static final String INVALID_EXPIRATION_DATE_ERROR = "Invalid expiration date.";
 
   /** A map of grocery objects. */
   private final HashMap<String, Grocery> groceries = new HashMap<>();
@@ -27,7 +33,7 @@ public class FoodStorage {
    *
    * @return a list of all grocery objects in the food storage.
    */
-  public List<Grocery> getGroceries() {
+  public List<Grocery> getAllGroceries() {
     return new ArrayList<>(groceries.values());
   }
 
@@ -38,9 +44,22 @@ public class FoodStorage {
    *
    * @return a list of all grocery objects in the food storage, sorted alphabetically by name.
    */
-  public List<Grocery> getAlphabeticalGroceries() {
+  public List<Grocery> getAllGroceriesAlphabetically() {
     List<Grocery> sortedGroceries = new ArrayList<>(groceries.values());
     sortedGroceries.sort(Comparator.comparing(Grocery::getName));
+    return sortedGroceries;
+  }
+
+  /**
+   * Returns a list of all grocery objects in the food storage, sorted by expiration date.
+   * <p>
+   * Objects are sorted in ascending order.
+   *
+   * @return a list of all grocery objects in the food storage, sorted by expiration date.
+   */
+  public List<Grocery> getAllGroceriesByExpirationDate() {
+    List<Grocery> sortedGroceries = new ArrayList<>(groceries.values());
+    sortedGroceries.sort(Comparator.comparing(grocery -> grocery.getBatches().getFirst().getExpirationDate()));
     return sortedGroceries;
   }
 
@@ -51,11 +70,30 @@ public class FoodStorage {
    * @return the Grocery object with the specified name.
    * @throws IllegalArgumentException if the grocery object does not exist,
    */
-  public Grocery getGrocery(String name) throws IllegalArgumentException {
+  public Grocery getGroceryByName(String name) throws IllegalArgumentException {
+    if (name == null || name.isBlank()) {
+      throw new IllegalArgumentException(INVALID_NAME_ERROR);
+    }
     if (!groceries.containsKey(name)) {
-      throw new IllegalArgumentException(NO_GROCERY_FOUND);
+      throw new IllegalArgumentException(NO_GROCERY_FOUND_ERROR);
     }
     return groceries.get(name);
+  }
+
+  /**
+   * Returns a list of all grocery objects in the food storage that belong to a specified category.
+   *
+   * @param category the category to filter the grocery objects by.
+   * @return a list of all grocery objects in the food storage that belong to the specified
+   *         category.
+   */
+  public List<Grocery> getGroceriesByCategory(String category) {
+    if (category == null || category.isBlank()) {
+      throw new IllegalArgumentException(INVALID_CATEGORY_ERROR);
+    }
+    return groceries.values().stream()
+        .filter(grocery -> grocery.getCategory().equalsIgnoreCase(category))
+        .toList();
   }
 
   /**
@@ -64,61 +102,53 @@ public class FoodStorage {
    * Does not return grocery objects that expire on the given date, only those that expire before.
    *
    * @param date the date to compare the expiration date of the grocery objects to.
-   * @return a list of all grocery objects that expire before the given date.
-   * @throws IllegalArgumentException if no grocery objects expire before the given date.
+   * @return a list of all grocery objects that expire before the given date. List can be empty.
+   *
+   * @throws IllegalArgumentException if the given date is null.
    */
-  public List<Grocery> getExpiringGroceries(LocalDate date) throws IllegalArgumentException {
-    List<Grocery> expiringGroceries = new ArrayList<>();
-    for (Grocery grocery : groceries.values()) {
-      GroceryBatch earliestExpBatch  = grocery.getBatches().getFirst();
-        if (earliestExpBatch.getExpirationDate().isBefore(date)) {
-          expiringGroceries.add(grocery);
-          break;
-        }
+  public List<Grocery> getGroceriesExpiringBeforeDate(LocalDate date) throws IllegalArgumentException {
+    if (date == null) {
+      throw new IllegalArgumentException(INVALID_EXPIRATION_DATE_ERROR);
     }
+    return groceries.values().stream()
+        .filter(grocery -> grocery.getBatches().getFirst().getExpirationDate().isBefore(date))
+        .toList();
+  }
 
-    if (expiringGroceries.isEmpty()) {
-      throw new IllegalArgumentException(NO_GROCERY_FOUND);
+  /**
+   * Returns a list of all grocery objects in the food storage that expire on a given date.
+   * <p>
+   * Does not return grocery objects that expire before the given date, only those that expire on
+   * the given date.
+   *
+   * @param date the date to compare the expiration date of the grocery objects to.
+   * @return a list of all grocery objects that expire on the given date. List can be empty.
+   *
+   * @throws IllegalArgumentException if the given date object is null.
+   */
+  public List<Grocery> getGroceriesExpiringOnDate(LocalDate date) throws IllegalArgumentException {
+    if (date == null) {
+      throw new IllegalArgumentException(INVALID_EXPIRATION_DATE_ERROR);
     }
-    else {
-      return expiringGroceries;
-      }
+    return groceries.values().stream()
+        .filter(grocery -> grocery.getBatches().getFirst().getExpirationDate().isEqual(date))
+        .toList();
   }
 
   /**
    * Adds a grocery object to the food storage.
-   * <p>
-   * If the food storage already contains the grocery object, the method will add all the batches of
-   * the new grocery object to the existing grocery object. If the food storage does not contain the
-   * grocery object, the method will add it.
    *
    * @param grocery the grocery object to add.
+   * @throws IllegalArgumentException if the grocery object already exists in the food storage.
    */
-  public void addGrocery(Grocery grocery) {
+  public void addGrocery(Grocery grocery) throws IllegalArgumentException {
+    if (grocery == null) {
+      throw new IllegalArgumentException(NULL_GROCERY_ERROR);
+    }
     if (groceries.containsKey(grocery.getName())) {
-      for (GroceryBatch batch : grocery.getBatches()) {
-        addGroceryBatch(grocery.getName(), batch);
-      }
-      return;
+      throw new IllegalArgumentException(GROCERY_EXISTS_ERROR);
     }
     groceries.put(grocery.getName(), grocery);
-  }
-
-  /**
-   * Removes the specified object from the food storage.
-   * <p>
-   * If the food storage does not contain the grocery object, the method will throw an
-   * {@code IllegalArgumentException}.
-   *
-   * @param grocery the grocery object.
-   * @throws IllegalArgumentException if the given grocery object does not exist in the food
-   *        storage.
-   */
-  private void removeGrocery(Grocery grocery) throws IllegalArgumentException {
-    if (!groceries.containsKey(grocery.getName())) {
-      throw new IllegalArgumentException(NO_GROCERY_FOUND);
-    }
-    groceries.remove(grocery.getName());
   }
 
   /**
@@ -132,10 +162,30 @@ public class FoodStorage {
    */
   public void addGroceryBatch(String name, GroceryBatch batch) throws IllegalArgumentException {
     if (!groceries.containsKey(name)) {
-          throw new IllegalArgumentException(NO_GROCERY_FOUND);
-      }
+      throw new IllegalArgumentException(NO_GROCERY_FOUND_ERROR);
+    }
     Grocery existingGrocery = groceries.get(name);
     existingGrocery.addBatch(batch);
+  }
+
+  /**
+   * Removes the specified object from the food storage.
+   * <p>
+   * If the food storage does not contain the grocery object, the method will throw an
+   * {@code IllegalArgumentException}.
+   *
+   * @param grocery the grocery object.
+   * @throws IllegalArgumentException if the given grocery object does not exist in the food
+   *        storage.
+   */
+  private void removeGrocery(Grocery grocery) throws IllegalArgumentException {
+    if (grocery == null) {
+      throw new IllegalArgumentException(NULL_GROCERY_ERROR);
+    }
+    if (!groceries.containsKey(grocery.getName())) {
+      throw new IllegalArgumentException(NO_GROCERY_FOUND_ERROR);
+    }
+    groceries.remove(grocery.getName());
   }
 
   /**
@@ -159,13 +209,13 @@ public class FoodStorage {
   public void consumeGrocery(Grocery grocery, double amount) throws IllegalArgumentException {
     String name = grocery.getName();
     if (!groceries.containsKey(name)) {
-      throw new IllegalArgumentException(NO_GROCERY_FOUND);
+      throw new IllegalArgumentException(NO_GROCERY_FOUND_ERROR);
     }
     if (amount <= 0) {
-      throw new IllegalArgumentException("Amount must be a positive number.");
+      throw new IllegalArgumentException(INVALID_AMOUNT_ERROR + "Must be a positive number.");
     }
     if (groceries.get(name).getTotalAmount() < amount) {
-      throw new IllegalArgumentException("Amount exceeds the current amount of the grocery");
+      throw new IllegalArgumentException(INVALID_AMOUNT_ERROR + "Exceeds the current amount of the grocery");
     }
     if (groceries.get(name).getTotalAmount() == amount) {
       removeGrocery(groceries.get(name));
